1d8e1c8059a6bcb98f016a8b1673a19b0dc0ec03
Vicky Junior <vickyjr88@gmail.com>
2023-03-17 12:50:51 -0700

    Fix an oversight bug.

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,10 +15,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	LastDigit = n % 10;
-	if (n == 0)
+	if (LastDigit == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, LastDigit);
-	} else if (n < 6)
+	} else if (LastDigit < 6)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n",
 			n,
