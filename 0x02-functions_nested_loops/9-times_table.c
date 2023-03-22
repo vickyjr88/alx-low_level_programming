f3c9257f073b74dc5b7f8de7d2cff5fa052b7f16
Vicky Junior <vickyjr88@gmail.com>
2023-03-21 18:14:23 -0700

    Take 5

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -25,31 +25,21 @@ void times_table(void)
 				if (y != 0)
 					_putchar(' ');
 				_putchar('0');
-				if (y < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
 			} else if (z > 0 && z < 10)
 			{
 				_putchar(' ');
 				_putchar(z + '0');
-				if (y < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
 			} else
 			{
 
 
 				_putchar((z / 10) + '0');
 				_putchar((z % 10) + '0');
-				if (y < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
+			}
+			if (y < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
 			}
 			y++;
 		}
