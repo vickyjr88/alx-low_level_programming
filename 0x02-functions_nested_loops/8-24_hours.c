9cfc667a8a572a45ac6f6c4ccca04918959876c4
Vicky Junior <vickyjr88@gmail.com>
2023-03-21 17:08:05 -0700

    8. I'm federal agent Jack Bauer, and today is the longest day of my life

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute
+ *
+ * Prints minutes
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int x = 0;
+
+	while (x < 24)
+	{
+		int y = 0;
+
+		while (y < 60)
+		{
+			if (x < 10)
+			{
+				_putchar('0');
+				_putchar('0' + x);
+			} else
+			{
+				_putchar((x / 10) + '0');
+				_putchar((x % 10) + '0');
+			}
+			_putchar(':');
+			if (y < 10)
+			{
+				_putchar('0');
+				_putchar('0' + y);
+			} else
+			{
+				_putchar((y / 10) + '0');
+                                _putchar((y % 10) + '0');
+			}
+			_putchar('\n');
+			y++;
+		}
+		x++;
+	}
+}
