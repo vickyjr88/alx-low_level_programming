7229e324e175df8f7a94884bcd72f9cfceb38411
Vicky Junior <vickyjr88@gmail.com>
2023-03-22 17:34:34 -0700

    Ile mnenos

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,21 +11,21 @@
 
 int main(void)
 {
-	int n1 = 1;
-	int n2 = 2;
-	int n3;
+	long n1 = 1;
+	long n2 = 2;
+	long n3;
 	int i;
 	int number = 50;
 
-	printf("\n%d, %d, ", n1, n2);
+	printf("%ld, %ld, ", n1, n2);
 
 	for (i = 2; i < number; ++i)
 	{
 		n3 = n1 + n2;
 		if (i == (number - 1))
-			printf(" %d\n", n3);
+			printf("%ld\n", n3);
 		else
-			printf(" %d, ", n3);
+			printf("%ld, ", n3);
 
 		n1 = n2;
 		n2 = n3;
