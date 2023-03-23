23f519dd7a6a1077b001080f56ea2ee2eace12ad
Vicky Junior <vickyjr88@gmail.com>
2023-03-22 17:55:27 -0700

    Working like charm

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -14,15 +14,17 @@ int main(void)
 	long n1 = 1;
 	long n2 = 2;
 	long n3 = 0;
-	long sum = 0;
+	long sum = 2;
 
-	while (n3 < 400000)
+	while (n3 <= 4000000)
 	{
 		n3 = n1 + n2;
 
 		n1 = n2;
 		n2 = n3;
-		sum += n3;
+
+		if ((n3 % 2) == 0)
+			sum += n3;
 	}
 	printf("%ld\n", sum);
 	return (0);
