7a6908947e3e95f759b092f3ceee337aaa46cefd
Vicky Junior <vickyjr88@gmail.com>
2023-03-23 19:15:01 -0700

    Remove trailing space

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,13 +13,16 @@ int main(void)
 	while (x < 101)
 	{
 		if ((x % 3) == 0 && (x % 5) == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if ((x % 3) == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if ((x % 5) == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else
-			printf("%d ", x);
+			printf("%d", x);
+
+		if (x != 100)
+			printf(" ");
 		x++;
 	}
 	printf("\n");
