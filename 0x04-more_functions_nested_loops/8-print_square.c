ffc831c23ea392fb574cf3cbb7bc857d7fd9f0b9
Vicky Junior <vickyjr88@gmail.com>
2023-03-23 18:44:58 -0700

    8. You are so much sunshine in every square inchwq

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @n: number input
+ * Prints a square using #
+ * Return: void
+ */
+void print_square(int n)
+{
+	if (n > 0)
+	{
+		int x = 0;
+
+		while (x < n)
+		{
+			int y = 0;
+
+			while (y < n)
+			{
+				_putchar('#');
+				y++;
+			}
+
+			_putchar('\n');
+			x++;
+		}
+	}
+	_putchar('\n');
+}
